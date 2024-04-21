4cc5372fe3f390d3833065130541471737de1f51
Valène Renaud <valene.renaud@gmail.com>
2024-04-21 20:54:31 +0200

    feat(modal): add close functionality to modal 'X' button

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,23 +1,48 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+/////
+// CONSTANTS / VARIABLES
+/////
 
 // DOM Elements
+const nav = document.getElementById("myTopnav");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+/////
+// FUNCTIONS
+/////
+
+/**
+ * Toggle mobile navigation visibility
+ */ 
+function toggleNav() {
+  nav.classList.toggle("responsive");
+}
+
+/**
+ * launch modal form
+ */ 
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+/**
+ * Close modal
+ */
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+
+/**** Events listener ****/
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close the modal
+closeBtn.addEventListener("click", closeModal);
+
+// Toggle the navigation
+document.querySelector('.icon').addEventListener("click", toggleNav);
