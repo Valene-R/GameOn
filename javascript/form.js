e05ee86f4711a3aab6cc15900b8895116b78acd9
Valène Renaud <valene.renaud@gmail.com>
2024-05-06 21:35:41 +0200

    refactor(validation): improve real time error message display

--- javascript/form.js
+++ javascript/form.js
@@ -2,150 +2,177 @@
 // CONSTANTS / VARIABLES
 /////
 
-// DOM Elements
+// DOM Elements 
+// Get the form element
 const form = document.querySelector("form"); 
+
+// Get input fields by their 'id'
 const firstNameInput = document.getElementById("first"); 
 const lastNameInput = document.getElementById("last");
 const emailInput = document.getElementById("email");
 const birthdateInput = document.getElementById("birthdate");
 const tournamentsInput = document.getElementById("quantity");
+
+// Get all city radio buttons
 const cityRadios = document.querySelectorAll('input[name="location"]');
-const formContainers = document.querySelectorAll(".formData");
+
+// Get the checkboxes 
 const conditionsCheckBox = document.getElementById("checkbox1");
 const newsEventsCheckBox = document.getElementById("checkbox2");
 
+// Create an array of all input elements
+// Spread operator '...' is used to decompose cityRadios into individual elements
+const inputs = [firstNameInput, lastNameInput, emailInput, birthdateInput, tournamentsInput, ...cityRadios, conditionsCheckBox];
+
+// Validation constants
+const minimumAge = 18; // Define minimum age for inscription
+const maximumAge = 100; // Maximum age accepted to prevent input errors
+const regexNotDecimal = /^\d+$/; // Regex to check if the input value contains only digits
 
 
 /////
 // FUNCTIONS
 /////
-  
-/**
- * Validate form fields and show error messages if they're needed
- * @returns {boolean} the form is valid or not
- */
-function validate() {
-  let isValid = true;
-  const participantAge = calculateAge(new Date(birthdateInput.value));
-  const minimumAge = 18; // Define minimum age for inscription
-  const maximumAge = 100; // Maximum age accepted to prevent input errors
 
-  // Parse the value of the input field as an integer using base 10 (numeric system)
-  const tournamentsCount = parseInt(tournamentsInput.value, 10);
-  // Regex to check if the input value contains only digits
-  const regexNotDecimal = /^\d+$/;
-  
-  // Validate the first name: field not empty and minimum 2 characters
-  if (!firstNameInput.value.trim() || firstNameInput.value.length < 2) {
-    setError(firstNameInput, "Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
-    isValid = false;
-  } else {
-    clearError(firstNameInput);
-  }
+/** Validation Rules **/
+const validationRules = {
+	// Validate the first name: field not empty and minimum 2 characters
+	first: value => {
+		if (!value.trim() || value.length < 2) {
+			return "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+		}
+	},
 
-  // Validate the last name: field not empty and minimum 2 characters
-  if (!lastNameInput.value.trim() || lastNameInput.value.length < 2) {
-    setError(lastNameInput, "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-    isValid = false;
-  } else {
-    clearError(lastNameInput);
-  }
+	// Validate the last name: field not empty and minimum 2 characters
+	last: value => {
+		if (!value.trim() || value.length < 2) {
+			return "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+		}
+	},
 
-  // Validate the email address
-  if (!emailInput.value.trim() || !isValidEmail(emailInput.value)) {
-    setError(emailInput, "Veuillez entrer une adresse électronique valide.");
-    isValid = false;
-  } else {
-    clearError(emailInput);
-  }
+	// Validate the email address
+	email: value => {
+		if (!value.trim() || !isValidEmail(value)) {
+			return "Veuillez entrer une adresse électronique valide.";
+		}
+	},
 
-  // Validate the birthdate: fied not empty and minimum age
-  if (!birthdateInput.value.trim()) {
-    setError(birthdateInput, "Vous devez entrer votre date de naissance.");
-    isValid = false;
-  } else if (participantAge < minimumAge) {
-    setError(birthdateInput, `Vous devez avoir au moins ${minimumAge} ans pour vous inscrire.`);
-    isValid = false;
-  } else if (participantAge > maximumAge) {
-    setError(birthdateInput, "Veuillez vérifier votre date de naissance, elle semble incorrecte.");
-    isValid = false;
-  } else {
-    clearError(birthdateInput);
-  }
+	// Validate the birthdate: fied not empty, minimum age and maximum age
+	birthdate: value => {
+		const participantAge = calculateAge(new Date(value)); // Calculate the age based on the birthdate value
+		if (!value.trim()) {
+			return "Vous devez entrer votre date de naissance.";
+		} else if (participantAge < minimumAge) {
+			return `Vous devez avoir au moins ${minimumAge} ans pour vous inscrire.`;
+		} else if (participantAge > maximumAge) {
+			return "Veuillez vérifier votre date de naissance, elle semble incorrecte.";
+		}
+	},
 
-  // Validate the number of tournaments already participated: integer between 0 and 99
+	// Validate the number of tournaments already participated: integer between 0 and 99
   // Check if the input value is an integer using a regex to exclude decimals
-  if (!regexNotDecimal.test(tournamentsInput.value)) { 
-    setError(tournamentsInput, "Veuillez entrer un nombre entier, sans décimales.");
-    isValid = false;
-  } else if (tournamentsCount < 0 || tournamentsCount > 99) {
-    setError(tournamentsInput, "Veuillez entrer un nombre entier entre 0 et 99.");
-    isValid = false;
-  } else {
-    clearError(tournamentsInput); 
-  }
+	quantity: value => {
+		// Parse the input value as an integer using base 10 (numeric system)
+		const tournamentCount = parseInt(value, 10);
+		if (!regexNotDecimal.test(value)) {
+			return "Veuillez entrer un nombre entier, sans décimales.";
+		} else if (tournamentCount < 0 || tournamentCount > 99) {
+			return "Veuillez entrer un nombre entier entre 0 et 99.";
+		}
+	},
 
-  // Validate the selected city for tournament
+	// Validate the selected city for tournament
   // Convert cityRadios to array and check if a radio button is checked
-  const citySelected = Array.from(cityRadios).some(radio => radio.checked);
-  // Add 'id' to 6th container, index 5 of array
-  formContainers[5].id = "radio-group";
-  const citiesContainer = document.getElementById("radio-group");
+	location: () => {
+		if (!Array.from(cityRadios).some(radio => radio.checked)) {
+			return "Vous devez sélectionner une ville.";
+		}
+	},
 
-  if (!citySelected) {
-    setError(citiesContainer, "Vous devez sélectionner une ville.");
-    isValid = false;
-  } else {
-    clearError(citiesContainer);
-  }
+	// Validate the conditions checkbox
+	checkbox1: checked => {
+		if (!checked) {
+			return "Vous devez accepter les conditions d'utilisation.";
+		}
+	}
+};
 
-  // Validate the conditions checkbox
-  if (!conditionsCheckBox.checked) {
-    setError(conditionsCheckBox, "Vous devez accepter les conditions d'utilisation.");
-    isValid = false;
-  } else {
-    clearError(conditionsCheckBox);
-  }
-
-  return isValid;
-}
-  
 /**
-  * Set an error message for a form field
-  * @param {HTMLElement} input The input element
-  * @param {string} message The error message
-  */
+ * Validate a specific form field
+ * @param {HTMLElement} input The input element to validate
+ * @returns {boolean} The input field is valid or not
+ */
+function validateField(input) {
+	// Determine the appropriate value for the input type
+	const value = input.type === "checkbox" ? input.checked : input.value;
+	// Get the corresponding validation rule
+	const rule = validationRules[input.id] || validationRules[input.name];
+	// Execute rule() with the value as argument
+	const errorMessage = rule(value);
+
+	// If value doesn't pass validation, rule will return an error message
+	if (errorMessage) {
+		// Set error if validation failed
+		setError(input, errorMessage);
+		return false;
+	} else {
+		// Clear error if validation passed
+		clearError(input);
+		return true;
+	}
+}
+
+/**
+ * Validate the entire form
+ * @returns {boolean} The form is valid or not
+ */
+function validateForm() {
+	let isValid = true;
+	// Validate all fields and determine the total validity
+	inputs.forEach(input => {
+		if (!validateField(input)) {
+			isValid = false;
+		}
+	});
+	return isValid;
+}
+
+/**
+ * Set an error message for a form field
+ * @param {HTMLElement} input The input element
+ * @param {string} message The error message
+ */
 function setError(input, message) {
-  // Find the closest parent element with ".formData" class to organize error message
+	// Find the closest parent element with ".formData" to organize error message
   const formData = input.closest(".formData");
   // Set the error message
   formData.setAttribute("data-error", message);
   // Show the error message
   formData.setAttribute("data-error-visible", "true");
 }
-  
+
 /**
-  * Clear the error message for a form field
-  * @param {HTMLElement} input The input element
-  */
+ * Clear the error message for a form field
+ * @param {HTMLElement} input The input element
+ */
 function clearError(input) {
-  const formData = input.closest(".formData");
-  // Remove the error message attribute
+	const formData = input.closest(".formData");
+	// Remove the error message attribute
   formData.removeAttribute("data-error");
   // Hide the error message
   formData.removeAttribute("data-error-visible");
 }
-  
+
 /**
  * Validate an email format using regex
  * @param {string} email The email to check
  * @returns {boolean} True if the format is correct, else false 
  */
 function isValidEmail(email) {
-  // Define a regex pattern for a valid email format
-  const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  // Use the test() method to check if the email matches the regex pattern
-  return regexEmail.test(email);
+	// Define a regex pattern for a valid email format
+	const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+	// Use the test() method to check if the email matches the regex pattern
+	return regexEmail.test(email);
 }
 
 /**
@@ -154,21 +181,20 @@ function isValidEmail(email) {
  * @returns {number} The calculated age
  */
 function calculateAge(birthDate) {
-  // Create a new Date object containing today's date
-  const today = new Date();
-  // Convert birthDate to a Date object 
-  const birth = new Date(birthDate);
-  // Use getFullYear() to get the year and calculate the age
-  let calculatedAge = today.getFullYear() - birth.getFullYear();
-  // Use getMonth() to get the month and calculate the month difference
-  const monthDifference = today.getMonth() - birth.getMonth();
+	// Create a new Date object containing today's date
+	const today = new Date();
+	// Convert birthDate to a Date object
+	const birth = new Date(birthDate);
+	// Use getFullYear() to get the year and calculate the age
+	let calculatedAge = today.getFullYear() - birth.getFullYear();
+	 // Use getMonth() to get the month and calculate the month difference
+	const monthDifference = today.getMonth() - birth.getMonth();
 
-  // Adjust age if the current date is before the birthdate
-  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
-    calculatedAge--; // Decrement the age (subtract 1) if the birthday hasn't yet passed this year
-  }
-
-  return calculatedAge; // Return the current age
+	// Adjust age if the current date is before the birthdate
+	if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
+		calculatedAge--; // Decrement the age (subtract 1) if the birthday hasn't yet passed this year
+	}
+	return calculatedAge; // Return the current age
 }
 
 /**
@@ -177,36 +203,43 @@ function calculateAge(birthDate) {
  * @param {number} duration The duration to show the toast in milliseconds. Default is 3000 ms
  */
 function showToast(message, duration = 3000) {
-  const toaster = document.createElement("div");
-  toaster.className = "toaster";
-  toaster.innerText = message;
-  document.body.appendChild(toaster);
+	const toaster = document.createElement("div");
+	toaster.className = "toaster";
+	toaster.innerText = message;
+	document.body.appendChild(toaster);
 
-  // Set a timeout to initiate the fade-out after the specified duration
-  setTimeout(() => {
-    toaster.classList.add("fade-out");
-    // Remove the toast from the DOM after the fade-out transition ends
-    toaster.addEventListener("transitionend", () => 
-      toaster.remove()
-    );
-  }, duration);
+	// Set a timeout to initiate the fade-out after the specified duration
+	setTimeout(() => {
+		toaster.classList.add("fade-out");
+		// Remove the toast from the DOM after the fade-out transition ends
+		toaster.addEventListener("transitionend", () => 
+			toaster.remove()
+		);
+	}, duration);
 }
+
 
 /////
 // EVENT LISTENERS
 /////
-  
+
+// Event listeners for real time validation
+inputs.forEach(input => {
+	input.addEventListener("input", () => validateField(input));
+	input.addEventListener("change", () => validateField(input));
+});
+
 // Event listener for form submission
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // Use the toLowerCase() method to convert email to lowercase for validation
-  emailInput.value = emailInput.value.toLowerCase();
+	e.preventDefault();
+	// Use the toLowerCase() method to convert email to lowercase for validation
+	emailInput.value = emailInput.value.toLowerCase();
 
-  if (validate()) {
-    if (newsEventsCheckBox.checked) {
-      showToast("Vous serez prévenu(e) des prochains événements !");
-    }
-    showConfirmationModal();
-    form.reset();
-  } 
+	if (validateForm()) {
+		if (newsEventsCheckBox.checked) {
+			showToast("Vous serez prévenu(e) des prochains événements !");
+		}
+		showConfirmationModal();
+		form.reset();
+	}
 });
