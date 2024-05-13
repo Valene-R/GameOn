a5276e31eee25fa0917b462936ee804c0abc8b67
Valène Renaud <valene.renaud@gmail.com>
2024-05-13 19:00:49 +0200

    refactor(ui): enhance responsive design and burger menu for better UX

--- javascript/modal.js
+++ javascript/modal.js
@@ -9,6 +9,9 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 const modalContent = document.querySelector(".modal-body");
+const navList = document.querySelector(".nav-list");
+const iconBurger = document.querySelector(".icon");
+const mainContent = document.querySelector('main');
 
 // Create new DOM elements for confirmation modal
 const confirmationModal = document.createElement("div");
@@ -19,7 +22,7 @@ const confirmationContent = document.createElement("div");
 confirmationContent.classList.add("content");
 
 const confirmationContainer = document.createElement("div");
-confirmationContainer.classList.add("modal-sucess-body");
+confirmationContainer.classList.add("modal-success-body");
 confirmationContainer.innerHTML = `
   <button class="close" aria-label="Fermer"></button>
   <h2><span>Merci pour</span><span> votre inscription</span></h2>
@@ -37,10 +40,22 @@ document.body.appendChild(confirmationModal);  // Add the confirmation modal at 
 /////
 
 /**
- * Toggle mobile navigation visibility
+ * Toggle mobile navigation visibility and manage burger menu interactions
  */ 
 function toggleNav() {
   nav.classList.toggle("responsive");
+  const isOpen = iconBurger.classList.toggle("toggle-nav");
+  
+  if (isOpen) {
+    const navHeight = navList.offsetHeight; // Calculate the height of the navigation list
+    document.body.classList.add("no-scroll"); // Disable scrolling on the body
+    mainContent.classList.add("overlay-effect"); // Add a visual effect to the main content
+    mainContent.style.marginTop = `${navHeight}px`; // Adjust the margin based on the height of the navigation
+  } else {
+    document.body.classList.remove("no-scroll");
+    mainContent.classList.remove("overlay-effect");
+    mainContent.style.marginTop = "0"; // Reset the margin of the main content to zero
+  }
 }
 
 /**
@@ -48,6 +63,11 @@ function toggleNav() {
  */ 
 function launchModal() {
   modalbg.style.display = "block";
+  // Prevent background interaction by disabling scrolling and clicks
+  document.body.classList.add("stop-effect-modalbg");
+
+  // Enable interactions within the modal
+  modalbg.style.pointerEvents = "auto";
 }
 
 /**
@@ -55,6 +75,7 @@ function launchModal() {
  */
 function closeModal() {
   modalbg.style.display = "none";
+  document.body.classList.remove("stop-effect-modalbg");
 }
 
 /**
@@ -63,6 +84,8 @@ function closeModal() {
 function showConfirmationModal() {
   modalbg.style.display = "none"; // Hide the modal form
   confirmationModal.style.display = "block";
+  document.body.classList.add("stop-effect-modalbg");
+  confirmationContent.style.pointerEvents = "auto"; // Detect clicks on the modal
 }
 
 /**
@@ -70,6 +93,7 @@ function showConfirmationModal() {
  */
 function closeConfirmationModal() {
   confirmationModal.style.display = "none";
+  document.body.classList.remove("stop-effect-modalbg");
 }
 
 
@@ -82,10 +106,16 @@ function closeConfirmationModal() {
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // Close the modal
-closeBtn.addEventListener("click", closeModal);
+closeBtn.addEventListener("click", () => {
+  closeModal(); 
+  form.reset(); // Reset the form fields
+  inputs.forEach(input => {
+    clearError(input); // Clear any error messages
+  });
+});
 
 // Toggle the navigation
-document.querySelector(".icon").addEventListener("click", toggleNav);
+iconBurger.addEventListener("click", toggleNav);
 
 // Close the confirmation modal if a button with '.close' or '.button' is clicked
 confirmationModal.addEventListener("click", (e) => {
